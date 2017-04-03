93b02c0f12361bd991b68c7bb3fc944653b42f67
sergioko737 <s29585476@gmail.com>
2017-04-03 17:40:31 +0300

    Add files sol.hh and sol.php just for play

---
+++ sol.hh
@@ -0,0 +1,16 @@
+<?hh
+
+
+
+function main(){
+
+  $arr = array(1,3,3,4,2);
+  echo "Hello worlddd!";
+  echo $arr[0];
+  foreach ($arr as $value){
+    echo "v ";
+    echo  $value;
+  }
+}
+
+main();
---
+++ sol.php
@@ -0,0 +1,4 @@
+<?hh
+
+    echo "Hello world!";
+    
