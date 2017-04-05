0287e6b1009f67330cefdb913d432598d18f6562
sergioko737 <s29585476@gmail.com>
2017-04-06 01:48:53 +0300

    Changes 2

--- sol.hh
+++ sol.hh
@@ -6,10 +6,11 @@ function main(){
 
   $arr = array(1,3,3,4,2);
   echo "Hello worlddd!";
-  echo $arr[0];
+  //echo $arr[0];
+  $product = array_product($arr);
   foreach ($arr as $value){
-    echo "v ";
-    echo  $value;
+    echo " ";
+    echo $product/$value;
   }
 }
 
---
+++ solutions.hh
@@ -0,0 +1,25 @@
+<?hh // strict
+
+function findMaxSliceSum(ConstVector<int> $xs): int
+{
+    throw new \Exception('Not implemented.');
+}
+
+function prodExcept(ConstVector<num> $xs): ConstVector<num>
+{
+    throw new \Exception('Not implemented.');
+}
+
+function parse(string $str): ?Set<string>
+{
+    throw new \Exception('Not implemented.');
+}
+
+/*function fanout<Tx, Ty, Tz>(
+    //(function (): Result<Tx>) $f,
+    //(function (Tx): Result<Ty>) $g1,
+    //(function (Tx): Result<Tz>) $g2
+);//: Result<(Ty, Tz)>
+{
+    throw new \Exception('Not implemented.');
+}*/
