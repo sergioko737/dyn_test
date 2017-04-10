16621111da59358562d4eb3be262dc984f46698b
sergioko737 <s29585476@gmail.com>
2017-04-10 18:40:42 +0300

    Started Problem 3 solution

--- solutions.hh
+++ solutions.hh
@@ -25,11 +25,35 @@ function prodExcept(ConstVector<num> $xs): ConstVector<num> {
 
 }
 
-/*function parse(string $str): ?Set<string>
- {
- throw new \Exception('Not implemented.');
- }
- /*function fanout<Tx, Ty, Tz>(
+function parse(string $str): ?Set<string> {
+  throw new \Exception('Not implemented.');
+}
+
+function checkMatchingBraces(string $str): bool {
+  static $braces = '{';
+  static $closing_braces = '}';
+  $stack = 0;
+
+  for ($i = 0, $length = strlen($str); $i < $length; $i++){
+    $char = $str[$i];
+
+    if ($char == $braces){
+      $stack = $stack+1;
+      echo "-if- ". $stack;
+    } else if ($char == $closing_braces){
+      $stack = $stack-1;
+      echo "-else if- ". $stack;
+      if($stack < 0){
+        echo "Mismatched braces";
+        return false;
+      }
+    } else {
+      echo "else ";
+    }
+  }
+  return $stack == 0;
+}
+/*function fanout<Tx, Ty, Tz>(
  //(function (): Result<Tx>) $f,
  //(function (Tx): Result<Ty>) $g1,
  //(function (Tx): Result<Tz>) $g2
@@ -39,17 +63,20 @@ function prodExcept(ConstVector<num> $xs): ConstVector<num> {
  }*/
 
 function main() {
-  $vec_test1 = ImmVector {-1, -2, -3, -4, -5 };
+  $vec_test1 = ImmVector {-1, -2, 3, -4, 5};
   $vec_test = ImmVector {30, 3434, 4, 5, 2, 3, 3};
-  echo "Problem 1</br>";
+  $string_pr3 = "}{dd}{{dd}";
+  echo "Problem 1<br>";
   echo "max slice is ";
   echo findMaxSliceSum($vec_test1);
-  echo "</br><br>";
+  echo "<br><br>";
   echo "Problem 2</br>";
   $zx = prodExcept($vec_test);
   var_dump($vec_test);
-  echo "</br>";
+  echo "<br>";
   var_dump($zx);
-  echo "</br>";
+  echo "<br><br>";
+  echo "Problem 3<br>";
+  var_dump (checkMatchingBraces ($string_pr3));
 }
 main();
